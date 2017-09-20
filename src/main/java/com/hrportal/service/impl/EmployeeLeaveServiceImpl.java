e5102be207749cffd4c5a4340b39a53426a7d18d
Lalithleelavishnu <lalithleelavishnu@gmail.com>
2017-09-20 19:57:45 +0530

    end points code fix

--- src/main/java/com/hrportal/service/impl/EmployeeLeaveServiceImpl.java
+++ src/main/java/com/hrportal/service/impl/EmployeeLeaveServiceImpl.java
@@ -6,6 +6,7 @@ import com.hrportal.repository.EmployeeLeaveRepository;
 import com.hrportal.service.IEmployeeLeaveService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
@@ -13,6 +14,7 @@ import java.util.Objects;
 /**
  * Created by Lalith leela vishnu on 20-09-2017.
  */
+@Service
 public class EmployeeLeaveServiceImpl implements IEmployeeLeaveService {
 
     @Autowired
